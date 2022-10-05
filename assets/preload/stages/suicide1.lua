dd6786c202ac457b56597f70fd166a07c75694c3
Quesorraerestu <113881206+Quesorraerestu@users.noreply.github.com>
2022-10-05 09:08:53 -0500

    Add files via upload

---
+++ assets/preload/stages/suicide1.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	
+	makeLuaSprite('suicide1', 'suicide1', -350, -150);
+	addLuaSprite('suicide1',false)
+        setLuaSpriteScrollFactor('suicide1', 0.9, 0.9);
+end
+	
---
+++ assets/preload/stages/suicidebackground.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('tv stahe','tv stahe', -600, -300);
+	addLuaSprite('tv stahe',false)
+	setScrollFactor('tv stahe',0.9,0.9);
+	
+end
